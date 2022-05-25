c91db3f26426b0d3c80ccf36d851a1c7998d9a98
Matheus Farias de Oliveira Matsumoto <83461020+MatheusFarias03@users.noreply.github.com>
2022-05-25 17:27:35 -0300

    Cells can change from living to dead

--- JogoDaVida.c
+++ JogoDaVida.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void clear() {
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+}
+
 void printMatrix(char** myMatrix, int matrixSize) {
     
     for (int i = 0; i < matrixSize; i++) {
@@ -28,11 +36,8 @@ void inputMatrix(char** myMatrix, int inputSize) {
     int cellCount = 0;
 
     while (i > 0 && j > 0) {
-        #ifdef _WIN32
-        system("cls");
-        #else
-        system("clear");
-        #endif
+        
+        clear();
 
         // Print the data 
         printf("Cell count : %d", cellCount);
@@ -71,6 +76,51 @@ void inputMatrix(char** myMatrix, int inputSize) {
 
 }
 
+void countNeighbor(char** myMatrix, int inputSize, int** countNeighborMatrix) {
+
+    int count = 0;
+
+    for (int i = 1; i < inputSize; i++) {
+        for (int j = 1; j < inputSize; j++) {
+            
+            count = 0;
+            
+            if (myMatrix[i - 1][j - 1] == 'O')
+                count++;
+            if (myMatrix[i - 1][j] == 'O')
+                count++;
+            if (myMatrix[i - 1][j + 1] == 'O')
+                count++;
+            if (myMatrix[i][j - 1] == 'O')
+                count++;
+            if (myMatrix[i][j + 1] == 'O')
+                count++;
+            if (myMatrix[i + 1][j - 1] == 'O')
+                count++;
+            if (myMatrix[i + 1][j] == 'O')
+                count++;
+            if (myMatrix[i + 1][j + 1] == 'O')
+                count++;
+
+            countNeighborMatrix[i-1][j-1] = count;
+        }
+    }
+
+}
+
+void updateMatrix(char** myMatrix, int** countNeighborMatrix, int inputSize) {
+    for (int i = 0; i < inputSize; i++) {
+        for (int j = 0; j < inputSize; j++) {
+            if (myMatrix[i + 1][j + 1] == ' ' && countNeighborMatrix[i][j] == 3)
+                myMatrix[i + 1][j + 1] = 'O';
+            else if (myMatrix[i + 1][j + 1] == 'O' && countNeighborMatrix[i][j] == 2 || countNeighborMatrix[i][j] == 3)
+                myMatrix[i + 1][j + 1] = 'O';
+            else
+                myMatrix[i + 1][j + 1] = ' ';
+        }
+    }
+}
+
 int main()
 {
     // User input for matrix size
@@ -80,7 +130,7 @@ int main()
     scanf_s("%d", &inputSize);
     matrixSize = inputSize + 2; // Create borders (+2)
 
-    // Creating matrix
+    // Creating char matrix
     char** myMatrix = malloc(sizeof(char*) * matrixSize);
     for (int i = 0; i < matrixSize; i++) {
         myMatrix[i] = malloc(sizeof(char) * matrixSize);
@@ -89,11 +139,31 @@ int main()
         }
     }
 
+    // Creating int matrix
+    int** countNeighborMatrix = malloc(sizeof(int*) * inputSize);
+        for (int i = 0; i < inputSize; i++) {
+            countNeighborMatrix[i] = malloc(sizeof(int) * inputSize);
+            for (int j = 0; j < inputSize; j++) {
+                myMatrix[i][j] = 0;
+            }
+        }
+
     inputMatrix(myMatrix, inputSize);
-    printMatrix(myMatrix, matrixSize);
 
+    // Game running
+    int intInput = 2;
+    while (intInput > 0) {
+        printMatrix(myMatrix, matrixSize);
+        countNeighbor(myMatrix, inputSize, countNeighborMatrix);
+        updateMatrix(myMatrix, countNeighborMatrix, inputSize);
+        printf("\nType a number different from 0 to see the next output: ");
+        scanf_s("%d", &intInput);
+        //clear();
+    }
 
-
+    for (int i = 0; i < matrixSize; i++) {
+        free(myMatrix[i]);
+    }
     free(myMatrix);
     return 0;
 }
